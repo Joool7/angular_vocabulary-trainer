3fbaed41228f520b72db02e38fe3cace0076223a
Joel Schneider <>
2026-01-05 22:57:19 +0100

    tidy up app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { TrainerComponent } from './views/trainer/trainer';
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   pathMatch: 'full',
-  //   component: TrainerComponent,
-  // },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: TrainerComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TrainerComponent } from './views/trainer/trainer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TrainerComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
